074eb1780477dcb7f0938aac27e572bd292d7c49
icanq <icanatawijaya@gmail.com>
2023-10-02 18:19:01 +0700

    init fe

---
+++ js/index.js
@@ -0,0 +1,117 @@
+const canvas = document.querySelector("canvas");
+const secondsCount = document.querySelector(".seconds");
+const level = document.querySelector(".grade");
+const context = canvas.getContext("2d");
+const imgDimensions = { width: 247 * 1.2, height: 325 * 1.2 };
+
+
+const levels = {
+  5: "Sr Assistant",
+  10: "Jr Honoror",
+  15: "Master Honoror",
+  35: "S Tier Honoror",
+  65: "Junior Acolyte",
+  105: "Acolyte",
+  150: "Senior Acolyte",
+  250: "Priest",
+  450: "Sage",
+  650: "Hermit",
+  1000: "Senior Hermit",
+  1500: "CEO",
+  2500: "Pope",
+  3500: "Underlord",
+  4500: "Lord",
+  10500: "OverLord",
+  20500: "King",
+  30500: "Anunnaki"
+}
+
+const startTime = Date.now();
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+context.translate(window.innerWidth / 2, window.innerHeight / 2);
+
+const image = new Image();
+image.src = "./assets/icanq.png";
+
+const loopingIcanq = 40; 
+const offsetDistance = 120;
+let currentOffset = 0;
+
+const movementRange = 200
+
+const mouseOffset = {
+  x: 0,
+  y: 0
+}
+
+const movementOffset = {
+  x: 0,
+  y: 0
+}
+
+image.onload = () => {
+  startLooping();
+};
+
+window.onresize = () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  context.setTransform(1, 0, 0, 1, 0, 0); //Reset the canvas context
+  context.translate(window.innerWidth / 2, window.innerHeight / 2);
+};
+
+window.addEventListener('mousemove', onMouseMove)
+
+function draw(offset, loopCount) {
+
+  let currentPercentage = (loopingIcanq - loopCount) / loopingIcanq
+  context.drawImage(
+    image,
+    -imgDimensions.width / 2 - offset/2 + (movementOffset.x * currentPercentage),
+    -imgDimensions.height / 2 - offset/2 + (movementOffset.y * currentPercentage),
+    imgDimensions.width + offset,
+    imgDimensions.height + offset
+  );
+}
+
+function onMouseMove(e) {
+  mouseOffset.x = (e.clientX - window.innerWidth / 2) / window.innerWidth / 2 * movementRange
+  mouseOffset.y = (e.clientY - window.innerHeight / 2) / window.innerHeight / 2 * movementRange
+}
+
+function lerp(start, end, amount) {
+  return start*(1-amount)+end*amount
+}
+
+function loopDraw() {
+
+  movementOffset.x = lerp(movementOffset.x, mouseOffset.x, 0.05)
+  movementOffset.y = lerp(movementOffset.y, mouseOffset.y, 0.05)
+
+  for (let i = loopingIcanq; i >= 1; i--) {
+    draw(i * offsetDistance + currentOffset, i);
+  }
+
+  draw(offsetDistance, 1);
+
+  currentOffset++;
+  if (currentOffset >= offsetDistance) {
+    currentOffset = 0;
+  }
+
+  const newTime = Math.floor((Date.now() - startTime) / 1000);
+
+  secondsCount.innerText = newTime;
+
+  if(levels[newTime]) {
+    level.innerText = levels[newTime]
+  }
+
+  requestAnimationFrame(loopDraw);
+}
+
+function startLooping() {
+  requestAnimationFrame(loopDraw);
+}
---
+++ js/message.js
@@ -0,0 +1,43 @@
+const API_BASE_URL = "http://localhost:3000";
+
+async function fetchMessages() {
+  try {
+    const response = await fetch(`${API_BASE_URL}/messages`);
+    const data = await response.json();
+    const messageContainer = document.getElementById("message-container");
+    messageContainer.innerHTML = "";
+    data.data.forEach((message) => {
+      const newMessageList = document.createElement("li");
+      newMessageList.textContent = `${message.name}: ${message.message}`;
+      messageContainer.appendChild(newMessageList);
+    })
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function sendMessage() {
+  const name = document.getElementById("input-name").value;
+  const message = document.getElementById("input-message").value;
+
+  try {
+    const sendMessage = await fetch(`${API_BASE_URL}/messages`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ name, message })
+  })
+  const response = await sendMessage.json();
+
+  if (response.success) {
+    await fetchMessages(); // Refresh the message list after adding a new message
+  } else {
+    console.error(response.message);
+  }
+  } catch (error) {
+    console.error('Error adding message:', error);
+  }
+}
+
+fetchMessages()
